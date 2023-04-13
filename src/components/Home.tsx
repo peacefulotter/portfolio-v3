101ab78e039ec4f7e3c6cc7dabfac079501fa7bd
PeacefulOtter <zankro67@gmail.com>
2023-04-13 17:47:44 +0200

    fixed typo

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -7,7 +7,7 @@ const Home = () => {
         <div className='flex flex-col items-center -mt-28'>
            <TbPlant2 className='stroke-1 text-6xl text-[var(--text-color)]'/>
            <Title title='Nathan Maire' />
-           <div className="text-xl mt-2">Compute Science MSC. Student <span>@ <a href='https://www.epfl.ch/en/'>EPFL</a></span></div>
+           <div className="text-xl mt-2">Computer Science MSC. Student <span>@ <a href='https://www.epfl.ch/en/'>EPFL</a></span></div>
         </div>
     )
 }
